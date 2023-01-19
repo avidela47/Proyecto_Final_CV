e4755f49cab7f959466324c50c7bba72d6acdf97
Ariel Videla <ariel_videla@hotmail.com>
2023-01-19 17:50:44 -0300

    first commit

---
+++ root/assets/js/main.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector("button");
+btn.onclick = () => {
+    window.print();
+}
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon =  () => document.body.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
